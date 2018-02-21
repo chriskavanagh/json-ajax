6e291581c0212ff3838e7ce54e45c3aa79114221
Chris Kavanagh <ckava3@gmail.com>
2018-02-21 05:06:07 -0500

    js changes

--- js/dom.js
+++ js/dom.js
@@ -12,9 +12,8 @@ ourList.addEventListener("click", activateItem);
 function activateItem(e) {
     if (e.target.nodeName === "LI") {
         headline.innerHTML = e.target.innerHTML;
-        for (i=0; i < listItems.length; i++) {
-            // e.target.parentNode.children.length
-            listItems[i].classList.remove("active");
+        for (i=0; i < e.target.parentNode.children.length; i++) {
+            e.target.parentNode.children[i].classList.remove("active");
         }
         e.target.classList.add("active");
     }    
